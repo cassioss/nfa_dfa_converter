3ad253fe0dc2dfd81c5e506eedd513c6e9a39f29
cassioss <cassiosantossousa@gmail.com>
2014-05-13 22:36:25 -0300

    NFA->DFA Converter
    This is a project in Java for an epsilon-NFA->DFA Converter.

--- nfa_dfa_converter/src/converter/NFA_DFA_Converter.java
+++ nfa_dfa_converter/src/converter/NFA_DFA_Converter.java
@@ -29,23 +29,24 @@ public class NFA_DFA_Converter {
 			if (enfa[i][0].contains(reference_1) && enfa[i][1].contains("q")) {
 				eclose = state + "," + enfa[i][1];
 				reference_1 = enfa[i][1];
-				do{
+				do {
 					reference_2 = eclose;
-					for (int j=0; j<enfa.length; j++){
-						if(enfa[j][0].contains(reference_1) && enfa[j][1].contains("q")){
+					for (int j = 0; j < enfa.length; j++) {
+						if (enfa[j][0].contains(reference_1)
+								&& enfa[j][1].contains("q")) {
 							eclose += "," + enfa[j][1];
 							reference_1 = enfa[j][1];
 						}
 					}
-				}while (reference_2 != eclose);
+				} while (reference_2 != eclose);
 			}
 		}
 		return eclose;
 	}
 
-	/* Cálculo de eclose para o caso geral - método de referência */
+	/* General function for ECLOSE, used as our reference */
 
-	protected static String eclose_completa(String estado, String[][] enfa) {
+	protected static String eclose_complete(String estado, String[][] enfa) {
 		String eclose = estado;
 		if (!estado.contains(",")) {
 			eclose = eclose_simple(eclose, enfa);
@@ -59,7 +60,7 @@ public class NFA_DFA_Converter {
 		return no_repeat(eclose);
 	}
 
-	/* Para descobrir o estado inicial de um NFA */
+	/* Finds the initial state(s) of a NFA */
 
 	protected static String estado_inicial(String[][] enfa) {
 		String com_seta = "";
@@ -74,9 +75,9 @@ public class NFA_DFA_Converter {
 		return com_seta;
 	}
 
-	/* Para descobrir o estado final de um NFA */
+	/* Finds the final state of a NFA */
 
-	protected static String estado_final(String[][] enfa) {
+	protected static String final_state(String[][] enfa) {
 		String com_asterisco = "";
 		for (int i = 0; i < enfa.length; i++) {
 			if (enfa[i][0].contains("*")) {
@@ -90,9 +91,9 @@ public class NFA_DFA_Converter {
 		return com_asterisco;
 	}
 
-	/* Dada uma string, verificar então se ela é o início de um DFA ou não */
+	/* Verifies if a given string refers to an initial state of the DFA */
 
-	protected static boolean eh_dfa_inicial(String candidata, String[][] enfa) {
+	protected static boolean is_initial_state(String candidata, String[][] enfa) {
 		String inicio = estado_inicial(enfa);
 		boolean possui_seta = false;
 		if (candidata.contains(",")) {
@@ -108,52 +109,48 @@ public class NFA_DFA_Converter {
 		return possui_seta;
 	}
 
-	/* Dada uma string, verificar então se ela é o final de um DFA ou não */
+	/* Verifies if a given string refers to a final state of the DFA */
 
-	protected static boolean eh_dfa_final(String candidata, String[][] enfa) {
-		String fim = estado_final(enfa);
-		boolean possui_asterisco = false;
-		if (candidata.contains(",")) {
-			String[] dividida = candidata.split(",");
+	protected static boolean is_final_state(String candidate, String[][] enfa) {
+		String fim = final_state(enfa);
+		boolean has_asterisk = false;
+		if (candidate.contains(",")) {
+			String[] dividida = candidate.split(",");
 			for (int i = 0; i < dividida.length; i++) {
 				if (fim.contains(dividida[i]))
-					possui_asterisco = true;
+					has_asterisk = true;
 			}
 		} else {
-			if (fim.contains(candidata))
-				possui_asterisco = true;
+			if (fim.contains(candidate))
+				has_asterisk = true;
 		}
-		return possui_asterisco;
+		return has_asterisk;
 	}
 
 	/*
-	 * Para um dado valor na primeira coluna, acrescentar "-->" se for inicial
-	 * ou "*" se for final
+	 * Verifies the first column, adding "-->" for initial states and "*" for
+	 * final states
 	 */
 
-	protected static String primeira_coluna(String em_questao, String[][] enfa) {
-		if (eh_dfa_inicial(em_questao, enfa)){
-			if (eh_dfa_final(em_questao, enfa))
+	protected static String first_column(String em_questao, String[][] enfa) {
+		if (is_initial_state(em_questao, enfa)) {
+			if (is_final_state(em_questao, enfa))
 				return "-->*" + em_questao;
 			else
 				return "-->" + em_questao;
-		}
-		else {
-			if (eh_dfa_final(em_questao, enfa))
+		} else {
+			if (is_final_state(em_questao, enfa))
 				return "*" + em_questao;
 			else
 				return em_questao;
 		}
 	}
 
-	/*
-	 * Determinar o conjunto de estados atingidos numa transição de um
-	 * determinado símbolo
-	 */
+	/* Finds all the states a certain transition reaches */
 
 	protected static String destino(String origem, int posicao, String[][] enfa) {
 		String resultante = "";
-		String eclose = eclose_completa(origem, enfa);
+		String eclose = eclose_complete(origem, enfa);
 		String[] dividida = eclose.split(",");
 		for (String estado : dividida) {
 			for (int i = 0; i < enfa.length; i++) {
@@ -170,19 +167,16 @@ public class NFA_DFA_Converter {
 		if (!resultante.startsWith("q"))
 			return "";
 		else {
-			String proxima = eclose_completa(resultante, enfa);
+			String proxima = eclose_complete(resultante, enfa);
 			if (!states_dfa.contains(proxima))
 				states_dfa.add(proxima);
 			return proxima;
 		}
 	}
 
-	/*
-	 * Determina os estados atingidos por uma determinada transição
-	 * iterativamente
-	 */
+	/* Find, by iteration, the states that a certain transition reaches */
 
-	protected static void destinos(String estados, String[][] enfa) {
+	protected static void destinations(String estados, String[][] enfa) {
 		if (!states_dfa.contains(estados))
 			states_dfa.add(estados);
 		control.add(estados);
@@ -190,25 +184,25 @@ public class NFA_DFA_Converter {
 			String destino = destino(estados, j, enfa);
 			if (destino != "" && destino != null
 					&& (!control.contains(destino)))
-				destinos(destino, enfa);
+				destinations(destino, enfa);
 		}
 	}
 
-	/* Início da determinação acima, partindo do estado inicial da DFA */
+	/* Start of the previous determination, using the initial state of the NFA */
 
 	protected static void estados_da_dfa(String[][] enfa) {
-		String primeiro_estado = eclose_completa(estado_inicial(enfa), enfa);
-		destinos(primeiro_estado, enfa);
+		String first_state = eclose_complete(estado_inicial(enfa), enfa);
+		destinations(first_state, enfa);
 	}
 
-	/* Construção efetiva do DFA a partir de um NFA */
+	/* Effective construction of the DFA from its NFA */
 
 	protected static String[][] dfa(String[][] enfa) {
 		estados_da_dfa(enfa);
 		int linha = 0;
 		String[][] dfa = new String[states_dfa.size()][enfa[0].length - 1];
 		for (String s : states_dfa) {
-			dfa[linha][0] = primeira_coluna(s, enfa);
+			dfa[linha][0] = first_column(s, enfa);
 			for (int j = 2; j < enfa[0].length; j++)
 				dfa[linha][j - 1] = destino(s, j, enfa);
 			linha++;
@@ -233,18 +227,20 @@ public class NFA_DFA_Converter {
 			"c" };
 	protected static String[] nome_coluna2 = { "Estado", "a", "b", "c" };
 
-	protected static Transition_Table gui1 = new Transition_Table(nome_coluna1, nfa1);       //nfa1, nfa2, nfa3
-	protected static Transition_Table gui2 = new Transition_Table(nome_coluna2, dfa(nfa1));  //nfa1, nfa2, nfa3
+	protected static Transition_Table gui1 = new Transition_Table(nome_coluna1,
+			nfa1); // nfa1, nfa2, nfa3
+	protected static Transition_Table gui2 = new Transition_Table(nome_coluna2,
+			dfa(nfa1)); // nfa1, nfa2, nfa3
 
 	public static void main(String args[]) {
 		gui1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		gui1.setSize(600, 180);
 		gui1.setVisible(true);
-		gui1.setTitle("Tabela de transições do épsilon-NFA 3");  //1, 2, 3
+		gui1.setTitle("Tabela de transições do épsilon-NFA 3"); // 1, 2, 3
 		gui2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		gui2.setSize(600, 180);
 		gui2.setVisible(true);
-		gui2.setTitle("Tabela de transições do DFA 3");   //1, 2, 3
+		gui2.setTitle("Tabela de transições do DFA 3"); // 1, 2, 3
 
 	}
 
